494159f6173c7581a1e3cd6e2517636f58679493
Joel Gonzalez <754537@student.dusd.net>
2024-03-08 13:47:52 -0800

    empece el reto pero no esta bien

---
+++ p5UpllayercontrolGonzalez/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private float speed = 25.0f;
+    private float turnSpeed = 60.0f;
+    private float VerticalInput;
+   
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VerticalInput = Input.GetAxis("Vertical");
+        transform.position = plane.transform.position + offset;
+
+        transform.Translate(Vector3.forward * Time.deltaTime);
+
+        transform.Rotate(Vector3.left*speed * Time.deltaTime);
+        
+    }
+}
+
